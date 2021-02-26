3de4abc14575013797788e45fd418e7332b5a520
Erich Diesel <ediesel09@gmail.com>
2021-02-25 21:16:38 -0600

    Added mouseover events and resizable grid

--- sketch.js
+++ sketch.js
@@ -1,24 +1,35 @@
-console.log("Test")
+
+let gridSize = 10;
+
 
 const container = document.querySelector("#gridContainer");
-
 container.style.borderStyle = "solid";
+container.style.gridTemplateColumns = "1fr ".repeat(gridSize);
+container.style.gridTemplateRows = "1fr ".repeat(gridSize);
+
+console.log("1f ".repeat(4));
 
 let grid = [];
 
 
-for (let i=0; i<4; i++) {
+for (let i=0; i < gridSize; i++) {
 		grid.push([]);
-	for (let j=0; j<4; j++){
+	for (let j=0; j < gridSize; j++){
 		grid[i].push(document.createElement('div'));
 		grid[i][j].classList.add("pixel");
+		grid[i][j].addEventListener("mouseover", 
+				function(){this.classList.toggle("darkened");});
+		grid[i][j].addEventListener("mouseout", 
+				function(){this.classList.toggle("darkened");});		
+				
 		container.appendChild(grid[i][j]);
 	}
 }
 
-container.style.gridTemplateColumns = "repeat(4, 1fr)";
-container.style.gridTemplateRows = "repeat(4, 1fr)";
 
-container.appendChild(grid[0][0]);
+
 		
-		
+
+
+
+
